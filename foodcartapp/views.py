2e66cc268ad5745c4b891c982d44cdaeb68957b3
Sergey <sergey.bv26@gmail.com>
2023-02-16 11:25:26 +0300

    delete unused imports

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,16 +1,10 @@
-import json
-from pprint import pprint
-
-from django.core.exceptions import ValidationError
-from django.forms import model_to_dict
 from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from phonenumber_field.validators import validate_international_phonenumber
 
-from .models import Product, Order
+from .models import Product
 from .serializers import OrderSerializer
 
 
